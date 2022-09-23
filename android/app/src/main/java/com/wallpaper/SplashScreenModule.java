2d38471d4fe6a8cbc9a4c3d58430a0c225bbca47
anhnh153 <anhnh153@viettel.com.vn>
2022-09-23 13:46:40 +0700

    splash screen android - ios

---
+++ android/app/src/main/java/com/wallpaper/SplashScreenModule.java
@@ -0,0 +1,14 @@
+package com.wallpaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenModule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_module);
+    }
+}
